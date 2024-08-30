b3c8803a4f6bc8a1397dde2e27f5dd7a47333c76
gustavcampos <gustavop.campos2004@gmail.com>
2024-08-29 21:57:36 -0300

    2005 t question not complete

--- questions_sbc/2005/t.cpp
+++ questions_sbc/2005/t.cpp
@@ -1,7 +1,33 @@
+// TODO: Nao está correto!!!!!!!!!
+
 #include <iostream>
 
 using namespace std;
 
 int main() {
+    int inputs, result;
+    bool prev, cur;
+
+    while (1) {
+        cin >> inputs;
+        
+        if (inputs == 0) {break;}
+        result = 0;
+        prev = 1;
+
+        for (int i = 0; i < inputs; i++) {
+            cin >> cur;
+
+            if (!(cur + prev)) {
+                result++;
+                prev = 1;
+            } else {
+                prev = cur;
+            }
+        }
+
+        cout << result << endl;
+    }
+
     return 0;
 };
